e4855f46e85e5489ab1f17f54283d82cf4e51306
Aditya Asthana <adi.a.asthana@gmail.com>
2019-12-19 19:18:34 -0800

    Submit Unit1 assignment.

---
+++ Unit1/Assignment1_aasthana1164649.java
@@ -0,0 +1,15 @@
+/* Assignment 1
+ * Author: Aditya Asthana
+ */
+public class Assignment1_aasthana1164649 {
+	
+		//main method, start of program
+		 public static void main( String [] args ){
+			//my name
+			System.out.println("My name is Aditya Asthana.");
+			//my age
+			System.out.println("I am 14 years old.");
+			//my favorite food
+			System.out.println("My favorite food is cereal!");
+		 }
+}
