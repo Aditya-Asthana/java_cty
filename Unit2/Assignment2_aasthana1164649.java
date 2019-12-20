0e7c24866ce953205f1728b2b783523330503b61
Aditya Asthana <adi.a.asthana@gmail.com>
2019-12-19 19:18:50 -0800

    Submit Unit2 assignment.

---
+++ Unit2/Assignment2_aasthana1164649.java
@@ -0,0 +1,23 @@
+/* 
+ * Assignment 2
+ * Author: Aditya Asthana
+ */
+public class Assignment2_aasthana1164649 {
+    public static void main( String [] args ){
+        //print the pattern defined in the instructions
+        System.out.println("         *    ");
+        System.out.println("        * *   ");
+        System.out.println("       *   *  ");
+        System.out.println("      *     * ");
+        System.out.println("     **** ****");
+        System.out.println("        * *   ");
+        System.out.println("        * *   ");
+        System.out.println("        * *   ");
+        System.out.println("     **** ****");
+        System.out.println("      *     * ");
+        System.out.println("       *   *  ");
+        System.out.println("        * *   ");
+        System.out.println("         *    ");
+  }
+
+}
